726c1a8fd2e966f929da292386aca70cf79fb245
Edan Gortzak <edan.gortzak@berkeley.edu>
2025-01-09 21:19:19 -0800

    finished basic hero section, react-typed fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
-    return (
-        <div>
-            <Navbar/>
-            <Hero/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { ReactTyped } from "react-typed";
+import headshot from "../assets/headshot2.png";
+import Cards from "./Cards";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-darkslategray text-white">
+      <div className="flex flex-col md:flex-row items-center max-w-[1440px] mx-auto p-4">
+        <div className=" w-1/3 flex justify-center md:justify-start pr-20">
+          <img
+            src={headshot}
+            className="mx-auto max-w-full h-auto rounded-full"
+          />
+        </div>
+        <div className="md:w-2/3 w-full flex flex-col justify-center items-center md:items-start text-center md:text-left">
+          <div className="flex justify-center items-center">
+            <h1 className="md:text-5xl sm:text-4xl text-3xl font-bold py-4">
+              I am a...
+            </h1>
+
+            <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4"></p>
+            <ReactTyped
+              className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 text-[#4d2fd2]"
+              strings={[
+                "Software Engineer",
+                "Mobile Developer",
+                "Full-Stack Web Developer",
+                "College Student",
+              ]}
+              typeSpeed={40}
+              backSpeed={50}
+              loop
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <p className="md:text-3xl sm:text-2xl py-4">
+              My name is Edan Gortzak and I am an Electrical Engineering and
+              Computer Science Student at UC Berkeley interested in software
+              engineering, mobile and web development, ML/AI, data science, and
+              more.
+            </p>
+          </div>
+          <button className="bg-[#4d2fd2] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-black">
+            Contact Me
+          </button>
+        </div>
+      </div>
+      <Cards />
+    </div>
+  );
+};
+
+export default Home;
